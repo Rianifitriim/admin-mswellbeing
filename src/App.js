b2fe3aa42451c32c080290977c6aebfb61eec03e
Rianifitriim <59384629+Rianifitriim@users.noreply.github.com>
2022-01-08 14:25:12 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import './App.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Routes from './admin/routes';
+function App() {
+  return (
+    <div>
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
